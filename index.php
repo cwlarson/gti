3bc0f89828cfb7f18e34a1ea7697f9cae84ade30
Casey Larson <casey.larson@personalcapital.com>
2015-03-12 18:16:29 -0700

    no message

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+
+		$root = $_SERVER['DOCUMENT_ROOT'];
+		include($root.'/_resources/partials/head.html');
+		include($root.'/_resources/global-vars.php');
+	?>
+</head>
+
+<body>
+	<?php include($root.'/_resources/partials/nav.html'); ?>
+
+	<div class="container">
+		<!-- <h1>Guerra Technologies</h1> -->
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+		<p>aaa</p>
+	</div>
+
+	<?php 
+		include($root.'/_resources/partials/footer.html');
+		include($root.'/_resources/partials/scripts.html');
+	?>
+</body>
+</html>
